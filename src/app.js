c51e821fe506f471239076c54e6dedf47815af5c
alyson <consulialyson@gmail.com>
2022-07-05 21:11:38 -0300

    fix: spacing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
-
 import router from "./routes/index.js";
 import handleError from "./middlewares/errorHandlingMiddleware.js";
 
